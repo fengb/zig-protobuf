7e05a08af116e455a2dfd6351cd8de030cde71ef
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-08-14 08:33:38 -0500

    Basic scaffolding

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,32 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn marshal(comptime T: type, item: T) []u8 {
+    return [_]u8{};
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+pub fn unmarshal(comptime T: type, allocator: *std.mem.Allocator, bytes: []u8) T {
+    return T{};
+}
+
+test "marshalling" {
+    const Example = struct {
+        label: []const u8 = "",
+        @"type": ?i32 = 77,
+        reps: []const i64 = [_]i64{},
+
+        allocator: ?*std.mem.Allocator = null,
+    };
+
+    const start = Example{
+        .label = "hello",
+        .@"type" = 17,
+        .reps = [_]i64{ 1, 2, 3 },
+    };
+    const binary = marshal(Example, start);
+    const result = unmarshal(Example, std.heap.direct_allocator, binary);
+
+    testing.expectEqualSlices(u8, start.label, result.label);
+    testing.expectEqual(start.@"type", result.@"type");
+    testing.expectEqualSlices(i64, start.reps, result.reps);
 }
